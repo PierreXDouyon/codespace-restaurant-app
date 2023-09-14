f98e04e0074d3dd755e81cd043b582f3eeda8f2d
Pierre Douyon <pierredouyon514@gmail.com>
2023-09-14 14:16:47 -0400

    Import the built code in backend

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const app = express();
 const port = 3000;
 const routes = require("./routes/index.js");
 var cors = require("cors");
+const path = __dirname + '/dist/';
+app.use(express.static(path));
 app.use(cors({
-  origin: 'http://localhost:8080',
+  origin: '/',
   methods: 'GET,POST,PUT,DELETE',
   allowedHeaders: 'Content-Type,Authorization',
   credentials: true,
 }));
+
+app.get('*', function (req,res) {
+  res.sendFile(path + "index.html");
+});
+app.use(express.json({limit: '50mb'}));
 const bodyParser = require("body-parser");
 // parse application/json
 app.use(bodyParser.json());
